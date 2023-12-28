f85ad3dc217ae1121d5d5013a97e422b9dd72465
EyadAlazmeh2 <eyadoalazmeh@gmail.com>
2023-12-28 23:41:10 +0300

    final

--- components/PreviewModal.tsx
+++ components/PreviewModal.tsx
@@ -9,9 +9,7 @@ const PreviewModal = () => {
   const previewModal = usePreviewModal()
   const product = usePreviewModal((state) => state.data)
 
-  if(!product) {
-    return null
-  }
+  if(!product) return null
   return (
     <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
